9ce630cf5deebf1b4a8ff5c89ffe79ca8ea3297e
saifali <saifalik218@gmail.com>
2026-05-25 01:50:49 +0530

    update in VerifyUserIsAbleToSignIn class

--- Selenium2026/src/automationExercise/VerifyUserIsAbleToSignIn.java
+++ Selenium2026/src/automationExercise/VerifyUserIsAbleToSignIn.java
@@ -12,13 +12,12 @@ public class VerifyUserIsAbleToSignIn {
 	protected static String URL = "https://automationexercise.com/";
 	protected static WebDriver driver;
 	protected static By automationExercise = By.xpath("//img[@alt='Website for automation practice']");
-
+	protected static String className = VerifyUserIsAbleToSignIn.class.getSimpleName();
 	public static WebDriver newUserSignup() {
-		String className = VerifyUserIsAbleToSignIn.class.getSimpleName();
-		driver = PreCondition.openBrowser();
+		
+		driver = PreCondition.openBrowser(URL);
 		String targetURL = "https://automationexercise.com/login";
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get(URL);
 		try {
 			WebElement signUpLink = driver.findElement(By.xpath("//a[text()=' Signup / Login']"));
 			if (signUpLink.isDisplayed()) {
